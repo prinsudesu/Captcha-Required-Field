fea018e0460c2ee675617ea6df29a67c75e7e009
Prince G <princeeugenio@gmail.com>
2022-09-09 18:11:35 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,8 @@
+<form>
+  <!-- google recaptcha -->
+  <div class="g-recaptcha" data-sitekey="your-site-key"></div>
+  
+  <!-- h captcha -->
+  <div class="h-captcha" data-sitekey="your-site-key"></div>
+  <button type="submit"> Submit</button>
+</form>
